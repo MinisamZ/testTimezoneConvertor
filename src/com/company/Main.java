75850495eae14ff2b4e6164ac109cb7e53368770
Alexandr <minisam2468@gmail.com>
2022-03-07 02:18:31 +0600

    getListCityByZoneNameAndByCode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,16 @@ public class Main {
         List<City> city = webService.getListCityByCode("KZ");
         System.out.println(city.get(2).toString2());
         System.out.println(webService.getCityByCityName("London").toString2());
+        System.out.println("===============");
+        List<City> cities = webService.getListCityByZoneName("Asia");
+        for (City city1 : cities) {
+            System.out.println(city1.toString());
+        }
+        System.out.println("===============");
+        List<City> cities2 = webService.getListCityByZoneNameAndByCode("Asia", "kz");
+        for (City city2 : cities2) {
+            System.out.println(city2.toString());
+        }
         // write your code here
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 //        System.out.println(sdf.format(496803600));
--- src/com/company/WebService.java
+++ src/com/company/WebService.java
@@ -2,7 +2,9 @@ package com.company;
 
 import com.company.model.City;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class WebService {
 
@@ -13,7 +15,7 @@ public class WebService {
         return timeZoneDao.getListCityByCode(countryCodeLocal);
     }
 
-    public City getCityByCityName(String s){
+    public City getCityByCityName(String s) {
         List<City> ret = timeZoneDao.getListCity();
         for (City city : ret) {
             if (city.cityName.equals(s)) {
@@ -22,4 +24,27 @@ public class WebService {
         }
         return null;
     }
+
+    public List<City> getListCityByZoneName(String s) {
+        List<City> ret = timeZoneDao.getListCity();
+        List<City> result = new ArrayList<>();
+        for (City city : ret) {
+            if (city.zoneName.equals(s)) {
+                result.add(city);
+            }
+        }
+        return result;
+    }
+
+    public List<City> getListCityByZoneNameAndByCode(String zone, String code) {
+        List<City> ret = timeZoneDao.getListCity();
+        List<City> result = new ArrayList<>();
+        for (City city : ret) {
+            if (city.zoneName.equals(zone) && city.countryCode.equals(code.toUpperCase(Locale.ROOT))) {
+                result.add(city);
+            }
+        }
+        return result;
+    }
+
 }
